6a0c6bb69b08880a28097ff3bd2db1a94e23bb8a
Biba777 <72846435+Biba777@users.noreply.github.com>
2020-11-23 01:47:25 +0300

    Add files via upload

---
+++ testCat.py
@@ -0,0 +1,13 @@
+from cat import Cat
+
+cat1 = Cat('Барон', 'Мальчик', 2)
+cat2 = Cat('Cэм', 'Мальчик', 2)
+
+print(cat1.name)
+print(cat1.sex)
+print(cat1.age)
+print('---')
+print(cat2.name)
+print(cat2.sex)
+print(cat2.age)
+
